e30519e75eed256d5a4ef9418d85f0c9e8b097d8
qa-mpeters <mike.peters@qualityaspirators.com>
2024-04-17 12:02:41 -0500

    Initial code commit

---
+++ truefit-textbox/truefit-textbox/TrueFitTextBox.cs
@@ -0,0 +1,13 @@
+﻿namespace truefit_textbox
+{
+    public partial class TrueFitTextBox : TextBox
+    {
+        public TrueFitTextBox()
+        {
+            BackColor = Color.Pink;
+            ForeColor = Color.Black;
+        }
+
+        public bool HasDecimal { get; set; }
+    }
+}
